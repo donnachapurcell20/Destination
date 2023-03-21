aeba405cda430a4be16ecf7bc4c6147e6d438240
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-03-21 12:17:41 +0000

    Changed my MainActivity.java to use OpenStreetMap created a mapview with the mapcontroller zoom set to 9 and the center set to the geo point location I hard coded.
    Created a marker overlay set the position to the startpoint put an anchor on it and created a title for it
    
    Also add a road manager then set up the start and end points with an arraylist of the geopoints
    Retrieved the road between both points
    
    Built a polyline with the route shape
    
    added the polyline to the overlays of the map
    
    showed the route steps on the map
    
    filled the bubbles that will have the directions and information in them with images
    
    then added a refresh the map so the user can see these changes
    
    Made changes to the activity_main.xml
    Added two textboxes one for starting point and the other for end point and a search button which will search for the route and then show it on the map
    
    Made changes to the strings.xml
    Added directions strings
    Added the id's for textboxes and search button
    
    In the build.gradle I added to my dependencies
    implementation 'com.android.volley:volley:1.2.1'
                                          implementation 'androidx.coordinatorlayout:coordinatorlayout:1.2.0'
                                          implementation 'org.osmdroid:osmdroid-android:6.1.13'
                                          implementation 'org.osmdroid:osmdroid-wms:6.1.10'
                                          implementation 'org.osmdroid:osmdroid-mapsforge:6.1.10'
                                          implementation 'com.squareup.okhttp3:okhttp:4.9.3'
                                          implementation 'com.google.firebase:firebase-database:20.1.0'

--- app/src/main/java/com/example/destination/MainActivity.java
+++ app/src/main/java/com/example/destination/MainActivity.java
@@ -4,83 +4,112 @@ package com.example.destination;
 
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class MainActivity extends AppCompatActivity
-{
-
-
-    @Override public void onCreate(Bundle savedInstanceState)
-    {
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+import java.io.IOException;
+import java.util.List;
 
 
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private EditText startEditText;
+    private EditText endEditText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-
         setContentView(R.layout.activity_main);
-        MapView map = (MapView) findViewById(R.id.mapView);
-        map.setMultiTouchControls(true);
 
-        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
-        IMapController mapController = map.getController();
+        // Initialize views
+        mapView = findViewById(R.id.mapView);
+        startEditText = findViewById(R.id.start_point);
+        endEditText = findViewById(R.id.end_point);
+
+        // Set up map
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
         mapController.setZoom(9);
-        mapController.setCenter(startPoint);
-
-        //Marker Overlay
-        Marker startMarker = new Marker(map);
-        startMarker.setPosition(startPoint);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        startMarker.setTitle("Start point");
-
-//        map.invalidate();
-
-        //Road Manager
-        RoadManager roadManager = new OSRMRoadManager(this, "Donnacha/1.0");
-
-        //Setting up start and end point
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-        waypoints.add(startPoint);
-        GeoPoint endPoint = new GeoPoint(48.4, -1.9);
-        waypoints.add(endPoint);
-
-        //Retrieve the road between these points
-        Road road = roadManager.getRoad(waypoints);
-        if (road.mStatus != Road.STATUS_OK)
-            Toast.makeText(this, "Error when loading the road - status=" + road.mStatus, Toast.LENGTH_SHORT).show();
 
 
-        //Build a Polyline with the route shape
-        Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+        // Set up search button
+        Button searchButton = findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                String startLocation = startEditText.getText().toString();
+                String endLocation = endEditText.getText().toString();
+                if (!startLocation.isEmpty() && !endLocation.isEmpty()) {
+                    // Get start and end points from geocoding
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+                    List<Address> startAddresses = null;
+                    try {
+                        startAddresses = geocoder.getFromLocationName(startLocation, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    List<Address> endAddresses = null;
+                    try {
+                        endAddresses = geocoder.getFromLocationName(endLocation, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (!startAddresses.isEmpty() && !endAddresses.isEmpty()) {
+                        GeoPoint start = new GeoPoint(startAddresses.get(0).getLatitude(), startAddresses.get(0).getLongitude());
+                        GeoPoint end = new GeoPoint(endAddresses.get(0).getLatitude(), endAddresses.get(0).getLongitude());
 
-        //Add this Polyline to the overlays of the map
-        map.getOverlays().add(roadOverlay);
+                        // Add start and end points to waypoints
+                        ArrayList<GeoPoint> waypoints = new ArrayList<>();
+                        waypoints.add(start);
+                        waypoints.add(end);
 
-        //Showing the Route steps on the map
-        Drawable nodeIcon = getResources().getDrawable(R.drawable.img);
-        for (int i=0; i<road.mNodes.size(); i++){
-            RoadNode node = road.mNodes.get(i);
-            Marker nodeMarker = new Marker(map);
-            nodeMarker.setPosition(node.mLocation);
-            nodeMarker.setIcon(nodeIcon);
-            nodeMarker.setTitle("Step "+i);
-            map.getOverlays().add(nodeMarker);
+                        // Get road between start and end points
+                        RoadManager roadManager = new OSRMRoadManager(MainActivity.this, "Donnacha/1.0");
+                        Road road = roadManager.getRoad(waypoints);
+                        if (road.mStatus != Road.STATUS_OK) {
+                            Toast.makeText(MainActivity.this, "Error when loading the road - status=" + road.mStatus, Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Build a Polyline with the route shape
+                            Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
 
-            //Filling the bubbles
-            nodeMarker.setSnippet(node.mInstructions);
+                            // Add this Polyline to the overlays of your map
+                            mapView.getOverlays().add(roadOverlay);
 
-            nodeMarker.setSubDescription(Road.getLengthDurationText(this, node.mLength, node.mDuration));
+                            // Show the route steps on the map
+                            Drawable nodeIcon = getResources().getDrawable(R.drawable.img);
+                            for (int i = 0; i < road.mNodes.size(); i++) {
+                                RoadNode node = road.mNodes.get(i);
+                                Marker nodeMarker = new Marker(mapView);
+                                nodeMarker.setPosition(node.mLocation);
+                                nodeMarker.setIcon(nodeIcon);
+                                nodeMarker.setTitle("Step " + i);
+                                mapView.getOverlays().add(nodeMarker);
 
-            Drawable icon = getResources().getDrawable(R.drawable.ic_continue);
-            nodeMarker.setImage(icon);
-        }
-        //Refresh the map
-        map.invalidate();
-        }
+                                // Fill the bubbles
+                                nodeMarker.setSnippet(node.mInstructions);
+                                nodeMarker.setSubDescription(Road.getLengthDurationText(MainActivity.this, node.mLength, node.mDuration));
+                                Drawable icon = getResources().getDrawable(R.drawable.ic_continue);
+                                nodeMarker.setImage(icon);
+                            }
+
+                            // Refresh the map
+                            mapView.invalidate();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Could not find start or end location", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
 }
