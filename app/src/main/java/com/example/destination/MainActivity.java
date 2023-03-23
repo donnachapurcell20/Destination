1b76926781a6e6d22cc036084cbab4aec2e2fc83
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-03-23 19:19:32 +0000

    Created an xml file for speed van or speed camera locations need to figure out how to get the longitude and latitude and name of road onto Firebase
    Add images for the directions, start and end points too.
    
    Added the ability to hide the keyboard and better directions system into my MainActivity.java adds icons according to the maneuver the user is doing for example left, right, roundabout

--- app/src/main/java/com/example/destination/MainActivity.java
+++ app/src/main/java/com/example/destination/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.destination;
 
 
 
+import android.app.ProgressDialog;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
@@ -15,7 +17,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.destination.routing.OSRMRoadManager;
+import com.example.destination.routing.Road;
+import com.example.destination.routing.RoadManager;
+import com.example.destination.routing.RoadNode;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -24,92 +37,154 @@ public class MainActivity extends AppCompatActivity {
     private MapView mapView;
     private EditText startEditText;
     private EditText endEditText;
+    private Drawable nodeIcon;
 
-    @Override
+
+    private class SearchTask extends AsyncTask<String, Void, Road> {
+
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = ProgressDialog.show(MainActivity.this, "", "Searching route...");
+        }
+
+        @Override
+        protected Road doInBackground(String... locations) {
+            String startLocation = locations[0];
+            String endLocation = locations[1];
+            if (!startLocation.isEmpty() && !endLocation.isEmpty()) {
+                // Get start and end points from geocoding
+                Geocoder geocoder = new Geocoder(MainActivity.this);
+                List<Address> startAddresses = null;
+                try {
+                    startAddresses = geocoder.getFromLocationName(startLocation, 1);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                List<Address> endAddresses = null;
+                try {
+                    endAddresses = geocoder.getFromLocationName(endLocation, 1);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                if (!startAddresses.isEmpty() && !endAddresses.isEmpty()) {
+                    GeoPoint start = new GeoPoint(startAddresses.get(0).getLatitude(), startAddresses.get(0).getLongitude());
+                    GeoPoint end = new GeoPoint(endAddresses.get(0).getLatitude(), endAddresses.get(0).getLongitude());
+
+                    // Add start and end points to waypoints
+                    ArrayList<GeoPoint> waypoints = new ArrayList<>();
+                    waypoints.add(start);
+                    waypoints.add(end);
+
+                    // Get road between start and end points
+                    RoadManager roadManager = new OSRMRoadManager(MainActivity.this, "Donnacha/1.0");
+                    Road road = roadManager.getRoad(waypoints);
+
+                    return road;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Road road) {
+            super.onPostExecute(road);
+            progressDialog.dismiss();
+            if (road != null && road.mStatus == Road.STATUS_OK) {
+                // Build a Polyline with the route shape
+                Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
+
+                // Add this Polyline to the overlays of your map
+                mapView.getOverlays().add(roadOverlay);
+
+                // Add markers for each step of the route
+                for (int i = 0; i < road.mNodes.size(); i++) {
+                    RoadNode node = road.mNodes.get(i);
+                    Marker nodeMarker = new Marker(mapView);
+                    nodeMarker.setPosition(node.mLocation);
+
+                    // Set the icon according to the maneuver
+                    switch (node.mManeuverType) {
+                        case RoadNode.MANEUVER_LEFT:
+                            nodeMarker.setIcon(getResources().getDrawable(R.drawable.ic_turn_left));
+                            break;
+                        case RoadNode.MANEUVER_RIGHT:
+                            nodeMarker.setIcon(getResources().getDrawable(R.drawable.ic_turn_right));
+                            break;
+                        case RoadNode.MANEUVER_ROUNDABOUT:
+                            // Use a different drawable for each roundabout exit
+                            int exitNumber = node.mInstructions.indexOf("exit");
+                            if (exitNumber >= 0 && exitNumber < node.mInstructions.length() - 4) {
+                                char exitChar = node.mInstructions.charAt(exitNumber + 5);
+                                int exitCount = Character.getNumericValue(exitChar);
+                                if (exitCount > 0 && exitCount <= 4) {
+                                    String drawableName = "ic_roundabout_" + exitCount;
+                                    int drawableId = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+                                    if (drawableId != 0) {
+                                        nodeMarker.setIcon(getResources().getDrawable(drawableId));
+                                        break;
+                                    }
+                                }
+                            }
+                            // Use the default roundabout drawable if the exit count cannot be determined
+                            nodeMarker.setIcon(getResources().getDrawable(R.drawable.ic_roundabout));
+                            break;
+                        default:
+                            nodeMarker.setIcon(getResources().getDrawable(R.drawable.ic_continue));
+                            break;
+                    }
+
+                    nodeMarker.setTitle("Step " + i);
+                    nodeMarker.setSnippet(node.mInstructions);
+                    nodeMarker.setSubDescription(Road.getLengthDurationText(MainActivity.this, node.mLength, node.mDuration));
+                    mapView.getOverlays().add(nodeMarker);
+                }
+            }
+        }
+
+
+    }
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Initialize views
-        mapView = findViewById(R.id.mapView);
+        mapView = findViewById(R.id.map);
         startEditText = findViewById(R.id.start_point);
         endEditText = findViewById(R.id.end_point);
-
-        // Set up map
-        mapView.setMultiTouchControls(true);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.getController().setZoom(10);
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        nodeIcon = getResources().getDrawable(R.drawable.ic_continue);
+        // Set the default location to London
         IMapController mapController = mapView.getController();
-        mapController.setZoom(9);
+        mapController.setZoom(12.0);
+        GeoPoint startPoint = new GeoPoint(53.0996218803593, -7.911131504579704);
+        mapController.setCenter(startPoint);
 
-
-        // Set up search button
         Button searchButton = findViewById(R.id.search_button);
-        searchButton.setOnClickListener(new View.OnClickListener()
-        {
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 String startLocation = startEditText.getText().toString();
                 String endLocation = endEditText.getText().toString();
-                if (!startLocation.isEmpty() && !endLocation.isEmpty()) {
-                    // Get start and end points from geocoding
-                    Geocoder geocoder = new Geocoder(MainActivity.this);
-                    List<Address> startAddresses = null;
-                    try {
-                        startAddresses = geocoder.getFromLocationName(startLocation, 1);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    List<Address> endAddresses = null;
-                    try {
-                        endAddresses = geocoder.getFromLocationName(endLocation, 1);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (!startAddresses.isEmpty() && !endAddresses.isEmpty()) {
-                        GeoPoint start = new GeoPoint(startAddresses.get(0).getLatitude(), startAddresses.get(0).getLongitude());
-                        GeoPoint end = new GeoPoint(endAddresses.get(0).getLatitude(), endAddresses.get(0).getLongitude());
-
-                        // Add start and end points to waypoints
-                        ArrayList<GeoPoint> waypoints = new ArrayList<>();
-                        waypoints.add(start);
-                        waypoints.add(end);
-
-                        // Get road between start and end points
-                        RoadManager roadManager = new OSRMRoadManager(MainActivity.this, "Donnacha/1.0");
-                        Road road = roadManager.getRoad(waypoints);
-                        if (road.mStatus != Road.STATUS_OK) {
-                            Toast.makeText(MainActivity.this, "Error when loading the road - status=" + road.mStatus, Toast.LENGTH_SHORT).show();
-                        } else {
-                            // Build a Polyline with the route shape
-                            Polyline roadOverlay = RoadManager.buildRoadOverlay(road);
-
-                            // Add this Polyline to the overlays of your map
-                            mapView.getOverlays().add(roadOverlay);
-
-                            // Show the route steps on the map
-                            Drawable nodeIcon = getResources().getDrawable(R.drawable.img);
-                            for (int i = 0; i < road.mNodes.size(); i++) {
-                                RoadNode node = road.mNodes.get(i);
-                                Marker nodeMarker = new Marker(mapView);
-                                nodeMarker.setPosition(node.mLocation);
-                                nodeMarker.setIcon(nodeIcon);
-                                nodeMarker.setTitle("Step " + i);
-                                mapView.getOverlays().add(nodeMarker);
-
-                                // Fill the bubbles
-                                nodeMarker.setSnippet(node.mInstructions);
-                                nodeMarker.setSubDescription(Road.getLengthDurationText(MainActivity.this, node.mLength, node.mDuration));
-                                Drawable icon = getResources().getDrawable(R.drawable.ic_continue);
-                                nodeMarker.setImage(icon);
-                            }
-
-                            // Refresh the map
-                            mapView.invalidate();
-                        }
-                    } else {
-                        Toast.makeText(MainActivity.this, "Could not find start or end location", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                new SearchTask().execute(startLocation, endLocation);
             }
         });
+    }
+    @Override
+    protected void onPause() {
+
+        // hide the keyboard in order to avoid getTextBeforeCursor on inactive InputConnection
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        inputMethodManager.hideSoftInputFromWindow(startEditText.getWindowToken(), 0);
+        inputMethodManager.hideSoftInputFromWindow(endEditText.getWindowToken(), 0);
+
+        super.onPause();
     }
 }
