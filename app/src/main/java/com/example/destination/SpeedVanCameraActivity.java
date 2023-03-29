3886b4ed3698692b96caf18520b3c52b61e3b9e3
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-03-29 22:10:26 +0100

    Updated google-services.json file Added firebase implementation Added the basics to upload information to Firebase

--- app/src/main/java/com/example/destination/SpeedVanCameraActivity.java
+++ app/src/main/java/com/example/destination/SpeedVanCameraActivity.java
@@ -12,25 +12,27 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
-public class SpeedVanCameraActivity extends AppCompatActivity {
+public class SpeedVanCameraActivity extends AppCompatActivity
+{
 
-    private EditText latitudeTextView;
-    private EditText longitudeTextView;
-    private Spinner spinnercategories;
-
-    // Declare a reference to your Firebase database
-    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,23 +40,18 @@ public class SpeedVanCameraActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_for_me);
 
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // Get the TextViews from the layout
-        longitudeTextView = findViewById(R.id.longitudeme);
-        latitudeTextView = findViewById(R.id.latitudeme);
-        spinnercategories = findViewById(R.id.spinner_categories);
-
-
         // Create a new user with a first and last name
-        Map<String, Object> speedVanCamera = new HashMap<>();
-        speedVanCamera.put("first", "Ada");
-        speedVanCamera.put("longitude", "Lovelace");
-        speedVanCamera.put("latitude", 1815);
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
 
-        // Add a new document with a generated ID
+// Add a new document with a generated ID
         db.collection("users")
-                .add(speedVanCamera)
+                .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
@@ -68,23 +65,49 @@ public class SpeedVanCameraActivity extends AppCompatActivity {
                     }
                 });
 
-    }
+//        // Create a new user with a first, middle, and last name
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Alan");
+//        user.put("middle", "Mathison");
+//        user.put("last", "Turing");
+//        user.put("born", 1912);
+//
+//// Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+//
 
-    public void onSubmitButtonClick(View view) {
-        // Get the selected category from the spinner
-        String category = spinnercategories.getSelectedItem().toString();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
-        // Get the longitude and latitude from the TextViews
-        double longitude = Double.parseDouble(longitudeTextView.getText().toString());
-        double latitude = Double.parseDouble(latitudeTextView.getText().toString());
 
-        // Create a new Firebase database reference for the data you want to store
-        DatabaseReference markerRef = mDatabase.child("your_node_name").push();
 
-        // Set the values for the new marker
-        markerRef.child("category").setValue(category);
-        markerRef.child("longitude").setValue(longitude);
-        markerRef.child("latitude").setValue(latitude);
+
+
     }
 }
 
