80dd971c2c23b6cbc98c646651c23b3b4d10efcd
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-04-12 18:37:37 +0100

    Commented code

--- app/src/main/java/com/example/destination/module/Distance.java
+++ app/src/main/java/com/example/destination/module/Distance.java
@@ -2,9 +2,11 @@ package com.example.destination.module;
 
 public class Distance
 {
+    //Declare variables
     public String text;
     public int value;
 
+    //Constructor takes two parameters string and an int
     public Distance(String text, int value) {
         this.text = text;
         this.value = value;
