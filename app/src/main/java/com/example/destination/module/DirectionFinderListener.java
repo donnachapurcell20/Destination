83ed6909ea7c107cf06a3c31ffe6d841499665b3
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-04-12 18:29:27 +0100

    Commented DirectionFinderListener.java

--- app/src/main/java/com/example/destination/module/DirectionFinderListener.java
+++ app/src/main/java/com/example/destination/module/DirectionFinderListener.java
@@ -3,8 +3,11 @@ package com.example.destination.module;
 import java.util.List;
 
 
-
+//Interface that defines two methods onDrectionFinderStart and onDirectionFinderSuccess
 public interface DirectionFinderListener {
+    // This method is called when the direction finding process starts.
     void onDirectionFinderStart();
+    //Method called when the direction finding process is successfully completed.
+    //Takes a list of route objects which represents the direction between two point
     void onDirectionFinderSuccess(List<Route> route);
 }
