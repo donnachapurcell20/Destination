c181bbb9db5403523b9a50f64416dd652f5ab1d4
Donnacha Purcell <donnachapurcell20@gmail.com>
2023-04-12 18:42:17 +0100

    Commented code

--- app/src/main/java/com/example/destination/module/Duration.java
+++ app/src/main/java/com/example/destination/module/Duration.java
@@ -1,9 +1,11 @@
 package com.example.destination.module;
 
 public class Duration {
+    //Declare variables
         public String text;
         public int value;
 
+        //Constructor with two parameters string and int
         public Duration(String text, int value) {
             this.text = text;
             this.value = value;
